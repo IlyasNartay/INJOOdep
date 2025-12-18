c1e731c671e13e0efae32315565c9daa40423593
Ilyas Nartay <ilyas.nartay@gmail.com>
2025-12-19 03:18:39 +0500

    fix sitemap

--- backend/app/routers/sitemap.py
+++ backend/app/routers/sitemap.py
@@ -1,53 +1,58 @@
-from fastapi import APIRouter
-from fastapi.responses import Response
+from fastapi import APIRouter, Response
 from datetime import datetime
-from app import schemas, services
+from app.database import SessionLocal
+from app import services, schemas
 
 router = APIRouter(tags=["SEO"])
 
-@router.get("/sitemap.xml", include_in_schema=False,)
+
+@router.get("/sitemap.xml", include_in_schema=False)
 def sitemap():
     db = SessionLocal()
     try:
+        # Получаем все блюда через сервис
         dishes = services.dish_service.get_all_dishes(db)
 
-        xml = """<?xml version="1.0" encoding="UTF-8"?>"""
-        xml += """
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-                xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
-        """
+        # Формируем XML
+        xml = '<?xml version="1.0" encoding="UTF-8"?>'
+        xml += '''
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+'''
 
-        # статические страницы
+        # Статические страницы
         pages = [
             ("https://injoo.kz/", "1.0"),
             ("https://injoo.kz/menu", "0.9"),
         ]
 
         for loc, priority in pages:
-            xml += f"""
-            <url>
-                <loc>{loc}</loc>
-                <changefreq>daily</changefreq>
-                <priority>{priority}</priority>
-            </url>
-            """
+            xml += f'''
+<url>
+    <loc>{loc}</loc>
+    <changefreq>daily</changefreq>
+    <priority>{priority}</priority>
+</url>
+'''
 
-        # динамика
+        # Динамические страницы (блюда)
         for dish in dishes:
-            xml += f"""
-            <url>
-                <loc>https://injoo.kz/dish/{dish.id}</loc>
-                <lastmod>{dish.updated_at.date()}</lastmod>
-                <priority>0.8</priority>
-                <image:image>
-                    <image:loc>{dish.image_url}</image:loc>
-                    <image:title>{dish.name}</image:title>
-                </image:image>
-            </url>
-            """
+            dish_serialized = schemas.DishRead.model_validate(dish)
+            lastmod = dish_serialized.updated_at.strftime(
+                "%Y-%m-%d") if dish_serialized.updated_at else datetime.utcnow().strftime("%Y-%m-%d")
+            xml += f'''
+<url>
+    <loc>https://injoo.kz/dish/{dish_serialized.id}</loc>
+    <lastmod>{lastmod}</lastmod>
+    <priority>0.8</priority>
+    <image:image>
+        <image:loc>{dish_serialized.image_url}</image:loc>
+        <image:title>{dish_serialized.name}</image:title>
+    </image:image>
+</url>
+'''
 
-        xml += "</urlset>"
-
-        return Response(xml, media_type="application/xml")
+        xml += '</urlset>'
+        return Response(content=xml, media_type="application/xml")
     finally:
         db.close()
